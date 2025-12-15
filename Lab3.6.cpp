6776598439a7bf3bc256fbcbfe786d62dc1681e1
s6804062617121 <s6804062617121@email.kmutnb.ac.th>
2025-12-15 16:35:11 +0700

    Add files via upload

---
+++ Lab3.6.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int s;
+    scanf("%d", &s) ;
+    if (s>= 68){
+    if (s >= 85)
+    printf("A\n");
+    else if (s >= 75)
+    printf("B\n");
+    else if (s >= 68)
+    printf("C %d\n",75-s);
+	}
+	else {
+    if (s >= 55)
+    printf("D\n");
+    else if (s < 55)
+    printf("F\n");
+	}
+	
+	}
+   
+	
+
+    
+
+
+
+
+
